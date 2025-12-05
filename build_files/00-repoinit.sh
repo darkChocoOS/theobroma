47cb7019f7f62a5e7be1fe79ba3a262420404ade
Synbi <>
2025-12-05 02:06:25 -0600

    fix 00-repoinit.sh
    add -y to all dnf commands

--- build_files/00-repoinit.sh
+++ build_files/00-repoinit.sh
@@ -7,34 +7,34 @@ dnf -y copr enable bieszczaders/kernel-cachyos
 dnf -y copr disable bieszczaders/kernel-cachyos
 
 # secureblue selinux policy
-dnf copr enable secureblue/selinux-policy
-dnf copr disable secureblue/selinux-policy
+dnf -y copr enable secureblue/selinux-policy
+dnf -y copr disable secureblue/selinux-policy
 
 # negativo17 fedora-multimedia
-dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
-dnf config-manager setopt fedora-multimedia.enabled=0
+dnf -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-multimedia.repo
+dnf -y config-manager setopt fedora-multimedia.enabled=0
 
 # negativo17 fedora-nvidia
-dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
-dnf config-manager setopt fedora-nvidia.enabled=0
+dnf -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
+dnf -y config-manager setopt fedora-nvidia.enabled=0
 
 # nvidia container toolkit
-dnf config-manager addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
-dnf config-manager setopt nvidia-container-toolkit.enabled=0
-dnf config-manager setopt nvidia-container-toolkit.gpgcheck=1
+dnf -y config-manager addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
+dnf -y config-manager setopt nvidia-container-toolkit.enabled=0
+dnf -y config-manager setopt nvidia-container-toolkit.gpgcheck=1
 
 # trivalent
-dnf config-manager addrepo --from-repofile=https://repo.secureblue.dev/secureblue.repo
-dnf config-manager setopt secureblue.enabled=0
+dnf -y config-manager addrepo --from-repofile=https://repo.secureblue.dev/secureblue.repo
+dnf -y config-manager setopt secureblue.enabled=0
 
 # terra
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf config-manager setopt terra-release.enabled=0
+dnf -y config-manager setopt terra-release.enabled=0
 
 # ublue packages
 # TODO: move away from these once possible
-dnf copr enable ublue-os/packages
-dnf copr disable ublue-os/packages
+dnf -y copr enable ublue-os/packages
+dnf -y copr disable ublue-os/packages
 
 # ublue flatpak
 dnf -y copr enable ublue-os/flatpak-test
