36d281297c552f53d965177fe438b00f1ecf3ffd
Synbi <>
2025-12-08 23:01:36 -0600

    please
    omit pcsc from dracut

--- build_files/99-cleanup.sh
+++ build_files/99-cleanup.sh
@@ -54,5 +54,5 @@ rm -rf /usr/bin/chsh
 
 KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
 export DRACUT_NO_XATTR=1
-dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --omit pcsc --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
