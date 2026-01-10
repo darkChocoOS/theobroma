fb3a32b552d7ace83c4bfd1b4e8f425a30f3a7e8
Synby <107513274+Synby@users.noreply.github.com>
2026-01-10 03:41:35 -0600

    this probably won't work

--- build_files/04-HWE.sh
+++ build_files/04-HWE.sh
@@ -7,6 +7,8 @@ for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
   rpm --erase $pkg --nodeps
 done
 
+dnf -y remove --no-autoremove kernel*
+
 pushd /usr/lib/kernel/install.d
 printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
 printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
